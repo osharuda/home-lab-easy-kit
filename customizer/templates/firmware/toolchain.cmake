817e83b08b8ac45e008791a9044580940f562231
osharuda <osharuda@github.com>
2023-09-02 19:24:34 +0300

    * Fixing template

--- customizer/templates/firmware/toolchain.cmake
+++ customizer/templates/firmware/toolchain.cmake
@@ -7,13 +7,13 @@ set(tools "/usr")
 
 # Should point to the CMSIS library. For the latest version of the library
 # this path should end with "/STM32F10x_StdPeriph_Lib_V3.6.0"
-set(STDPERIF_PATH "__STDPERIF_PATH__")
+set(STDPERIF_PATH "{__STDPERIF_PATH__}")
 
-set(CMAKE_C_COMPILER ${tools}/bin/arm-none-eabi-gcc)
-set(CMAKE_CXX_COMPILER ${tools}/bin/arm-none-eabi-g++)
-set(CMAKE_ASM_COMPILER  ${tools}/bin/arm-none-eabi-gcc)
-set(CMAKE_OBJCOPY ${tools}/bin/arm-none-eabi-objcopy)
-set(CMAKE_PRINT_SIZE ${tools}/bin/arm-none-eabi-size)
+set(CMAKE_C_COMPILER ${{tools}}/bin/arm-none-eabi-gcc)
+set(CMAKE_CXX_COMPILER ${{tools}}/bin/arm-none-eabi-g++)
+set(CMAKE_ASM_COMPILER  ${{tools}}/bin/arm-none-eabi-gcc)
+set(CMAKE_OBJCOPY ${{tools}}/bin/arm-none-eabi-objcopy)
+set(CMAKE_PRINT_SIZE ${{tools}}/bin/arm-none-eabi-size)
 
 
 set(CMAKE_EXE_LINKER_FLAGS "--specs=nosys.specs" CACHE INTERNAL "")
