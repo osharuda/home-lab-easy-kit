bba54d893241cedf722deee8b729c8eef417ab1d
osharuda <osharuda@github.com>
2024-07-28 19:31:30 +0200

    * Adding utils.sh as a library for bash scripts * Cosmetic modifications for build.sh

--- build.sh
+++ build.sh
@@ -3,19 +3,32 @@
 # In order to build without LIBHLEK set NO_LIBHLEK variable like in the example below:
 # NO_LIBHLEK=1 ./build.sh <.....>
 
+source ./utils.sh
+
 set -e
 INITDIR="$(pwd)"
 JOBCOUNT="$(nproc)"
 BUILDDIR="build"
 NCPU=$(grep -c ^processor /proc/cpuinfo)
 BUILDCONF="Debug"
+CLEAN_BUILD=0
 ROOTDIR=$(pwd)
 LOGFILE="${ROOTDIR}/build.log"
+FINISH_SUCCESS=0
+
+trap 'exit_handler ${FINISH_SUCCESS}' EXIT
 
 
 ######## Detect if we are working in different directory. In this case we should use symlink to mirror project in current directory
 HLEKDIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)"
 PROJECTDIR=$(pwd)
+
+function exit_handler() {
+	success=${1}
+	if [ ${FINISH_SUCCESS} == 0 ]; then
+		echo_box "${RED}F A I L U R E${NORMAL}" 13; 
+	fi
+}
 
 
 # Creates sym link
@@ -33,7 +46,6 @@ make_sym_link () {
     if [ -h ${dst_file} ]; then
         local link_path=$(readlink -f ${dst_file})
         if [ ${link_path} == ${src_file} ]; then
-            echo "${dst_file} is already created and ok."
             return
         fi
     fi
@@ -57,24 +69,22 @@ export -f make_sym_link
 # {2} - Target directory
 hlek_mirror () {
     if [ ${PROJECTDIR} == ${HLEKDIR} ]; then
-        echo "May not mirror HLEK into self."
+        echo_err "May not mirror HLEK into self."
         exit 1
     fi
 
+    echo_progess_begin "Mirroring HLEK project tree"
+
     local d='customizer'
-    echo "Creating symlinks for ${d}..."
     make_sym_link ${HLEKDIR} ${PROJECTDIR} ${d}
 
     local d='firmware'
-    echo "Creating symlinks for ${d}..."
     make_sym_link ${HLEKDIR} ${PROJECTDIR} ${d}
 
     local d='software'
-    echo "Creating symlinks for ${d}..."
     make_sym_link ${HLEKDIR} ${PROJECTDIR} ${d}
 
     local d='libhlek'
-    echo "Creating symlinks for ${d}..."
     mkdir -p ${d}
     make_sym_link ${HLEKDIR}/${d} ${PROJECTDIR}/${d} "src"
     local lhlek_inc="${d}/inc"
@@ -82,7 +92,6 @@ hlek_mirror () {
     find "${HLEKDIR}/${lhlek_inc}" ! -name '*_common.hpp' ! -name 'i2c_proto.h' -name "*.hpp" -exec bash -c "make_sym_link \"${HLEKDIR}/${lhlek_inc}\" \"${PROJECTDIR}/${lhlek_inc}\" '{}'" \;
 
     local d='hlekio'
-    echo "Creating symlinks for ${d}..."
     mkdir -p ${d}
     make_sym_link ${HLEKDIR}/${d} ${PROJECTDIR}/${d} "linux"
     make_sym_link ${HLEKDIR}/${d} ${PROJECTDIR}/${d} "environments"
@@ -94,6 +103,8 @@ hlek_mirror () {
 
     make_sym_link ${HLEKDIR} ${PROJECTDIR} global.json
     make_sym_link ${HLEKDIR} ${PROJECTDIR} customize.sh
+
+    echo_progess_ok
 }
 
 if [ ${PROJECTDIR} != ${HLEKDIR} ]; then
@@ -117,15 +128,23 @@ do
 		continue
 	fi
 
+	if [ ${ARG^^} == "CLEAN" ]
+	then
+		CLEAN_BUILD=1
+		continue
+	fi
+
 	JSONS[${i}]=${ARG}
 	((++i))
 done
 
 function make_subproject {
+	echo_progess_begin "CMake: ${CONFIGDIR}/$1/${BUILDCONF}"
 	echo "Generating cmake project for ${CONFIGDIR}/$1" >> "${LOGFILE}" 2>&1
 	cd ${CONFIGDIR} && cd $1 && mkdir ${BUILDCONF} && cd ${BUILDCONF}
 	cmake -DCMAKE_BUILD_TYPE=${BUILDCONF} -G "Ninja" ../ >> "${LOGFILE}" 2>&1
 	cd "${INITDIR}"
+	echo_progess_ok
 }
 
 function build_subproject {
@@ -142,28 +161,38 @@ function build_subproject {
 	else
 		make_subproject $1
 	fi
-	cd "${CONFIGDIR}/$1/${BUILDCONF}"
 
+	echo_progess_begin "Build: ${CONFIGDIR}/$1/${BUILDCONF}"
+	cd "${CONFIGDIR}/$1/${BUILDCONF}"
 	ninja -v -j${NCPU} >> "${LOGFILE}" 2>&1
+	echo_progess_ok
+
 	if [ $skip_install == false ]
 	then 
+		echo_wrn "Installing $1"
 		echo "Installing $1" >> "${LOGFILE}" 2>&1
 		sudo ninja install >> "${LOGFILE}" 2>&1
 	fi
 	cd "${INITDIR}"
+
 }
 
 
 function make_libhlek {
+    echo_progess_begin "CMake: libhlek/${BUILDCONF}"
     echo "Generating cmake project for LIBHLEK" >> "${LOGFILE}" 2>&1
     cd libhlek && mkdir ${BUILDCONF} && cd ${BUILDCONF}
     cmake -DCMAKE_BUILD_TYPE=${BUILDCONF} -G "Ninja" ../ >> "${LOGFILE}" 2>&1	
     cd ${INITDIR}
+    echo_progess_ok
 }
 
 function build_libhlek {
 	echo "Building LIBHLEK [${BUILDCONF}]" >> "${LOGFILE}" 2>&1
+
+	echo_progess_begin "Customizing: libhlek"
 	./customize.sh --libhlek --verbose >> "${LOGFILE}" 2>&1
+	echo_progess_ok
 
 	if [ -d "libhlek/${BUILDCONF}" ]
 	then
@@ -171,18 +200,24 @@ function build_libhlek {
 	else
 		make_libhlek
 	fi
+
+	echo_progess_begin "Build: libhlek/${BUILDCONF}"
 	cd "libhlek/${BUILDCONF}"
 	ninja -v -j${NCPU} >> "${LOGFILE}" 2>&1
+	echo_progess_ok
+	echo_wrn "Installing libhlek"
 	echo "Installing LIBHLEK" >> "${LOGFILE}" 2>&1
 	sudo ninja install >> "${LOGFILE}" 2>&1
 	cd "${INITDIR}"
 }
 
 function make_firmware {
+	echo_progess_begin "CMake: firmware/${BUILDCONF}"
 	echo "Generating cmake project for firmware/${CONFIGDIR}" >> "${LOGFILE}" 2>&1
 	cd ${CONFIGDIR} && cd firmware && mkdir ${BUILDCONF} && cd ${BUILDCONF}
 	cmake -G"Unix Makefiles" -DHLEK_ROOT="${INITDIR}" -DHLEK_CONFIG="${CONFIGDIR}" -DCMAKE_DEPENDS_USE_COMPILER=FALSE -DCMAKE_BUILD_TYPE=${BUILDCONF} -DCMAKE_TOOLCHAIN_FILE:PATH="../toolchain.cmake"  .. >> "${LOGFILE}" 2>&1
 	cd ${INITDIR}
+	echo_progess_ok
 }
 
 function build_firmware {
@@ -193,14 +228,34 @@ function build_firmware {
 	else
 		make_firmware
 	fi
+
+	echo_progess_begin "Build: firmware/${BUILDCONF}"
 	cd "${CONFIGDIR}/firmware/${BUILDCONF}"
 	make -j${NCPU} >> "${LOGFILE}" 2>&1
 	cd "${INITDIR}"
+	echo_progess_ok
+}
+
+function clean_build {
+	echo_progess_begin "Cleaning project build tree"
+	rm -rf "libhlek/${BUILDCONF}"
+	rm -rf hashes
+	for j in ${JSONS[@]}
+	do
+		local CONFIGDIR="${json%.*}"
+		rm -rf "${CONFIGDIR}"
+	done
+	echo_progess_ok
 }
 
 
 ####### Preparing log file
 rm -rf "${LOGFILE}"
+
+
+if [ ${CLEAN_BUILD} != 0 ]; then
+	clean_build
+fi
 
 # Build & install libhlek
 build_libhlek
@@ -220,7 +275,6 @@ do
 
 	# Build monitor
 	build_subproject "monitor"
-	
 
 	# Build example
 	build_subproject "example"
@@ -230,3 +284,7 @@ do
 done
 
 ./customize.sh --update-global
+
+FINISH_SUCCESS=1
+echo_box "${GREEN}S U C C E S S${NORMAL}" 13
+
---
+++ utils.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+
+BLACK=$(tput setaf 0)
+RED=$(tput setaf 1)
+GREEN=$(tput setaf 2)
+YELLOW=$(tput setaf 3)
+LIME_YELLOW=$(tput setaf 190)
+POWDER_BLUE=$(tput setaf 153)
+BLUE=$(tput setaf 4)
+MAGENTA=$(tput setaf 5)
+CYAN=$(tput setaf 6)
+WHITE=$(tput setaf 7)
+BRIGHT=$(tput bold)
+NORMAL=$(tput sgr0)
+BLINK=$(tput blink)
+REVERSE=$(tput smso)
+UNDERLINE=$(tput smul)
+
+ERR_PREFIX='${RED}🯀🯀🯀${NORMAL}  '
+WRN_PREFIX="${YELLOW}🯁🯂🯃${NORMAL}  "
+INF_PREFIX='🮌     '
+RESULT_OK="[${GREEN}✓${NORMAL}]"
+RESULT_FAIL="[${RED}𐄂${NORMAL}]"
+
+function echo_info() {
+	printf "${INF_PREFIX}${1}\n"
+}
+
+function echo_wrn() {
+	printf "${WRN_PREFIX}${1}\n"
+}
+
+function echo_err() {
+	printf "${ERR_PREFIX}${1}\n"
+}
+
+# param1: length of the line
+# param2: character to repeat
+function echo_line() {
+	local n=${1}
+	local s=${2}
+	local i=0
+
+	for (( ; i<${n}; i++ ))
+	do 
+		echo -n "${s}"
+	done
+}
+
+# param1 - text to print
+# param2 - optional, length of the text. Use it in the case of colour messages.
+function echo_box() {
+	local outer_offset=3
+	local inner_offset=3
+	local len=${#1}
+	local text=${1}
+
+	if [ ${2} ]; then
+		len=${2}
+	fi
+
+	# New line
+	echo ""
+
+	# Top line
+	echo_line ${outer_offset} ' '
+	echo -n '╒'
+	echo_line ${len}+${inner_offset}*2 '═'
+	echo '╕'
+
+	# Middle line
+	echo_line ${outer_offset} ' '
+	echo -n '│'
+	echo_line ${inner_offset} ' '
+	echo -n ${text}
+	echo_line ${inner_offset} ' '
+	echo '│'
+
+
+	# Bottom line
+	echo_line ${outer_offset} ' '
+	echo -n '╘'
+	echo_line ${len}+6 '═'
+	echo '╛'
+}
+
+# Prints first part of the messages like this:
+# Do somegning ......                  [OK]
+# <---------- PART 1 -----------------><P2>
+# param 1 - text to be printed
+# param 2 - length of the part 2. Optional, if not specified value from previous call is used.
+PROGRESS_PART1_LEN=40
+function echo_progess_begin() {
+	if [ ${2+1} ]; then 
+		PROGRESS_PART1_LEN=${2}
+	fi
+
+	local len=${#1}
+	echo -n "${1}"
+
+	if [ $len -lt $PROGRESS_PART1_LEN ]; then 
+		echo_line ${PROGRESS_PART1_LEN}-${len} " "
+	fi
+}
+
+function echo_progess_ok() {
+	echo "${RESULT_OK}"
+}
+
+function echo_progess_fail() {
+	echo "${RESULT_FAIL}"
+}
